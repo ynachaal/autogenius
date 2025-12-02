7b0f9c8df6f0b909122f78b1fb480c9c650800e8
Jatinabstain <jatin.abstain@gmail.com>
2025-12-02 10:47:45 +0530

    remove

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- DYNAMIC SEO METADATA -->
     <title>@yield('meta_title', config('settings.meta_title', 'AutoGenius Private Limited - The Automotive Testing Company'))</title>
@@ -10,15 +10,25 @@
     <meta name="keywords" content="@yield('meta_keywords', config('settings.meta_keywords', 'default, keywords, tags'))">
     <!-- END SEO -->
 
+    <link rel="stylesheet" href="{{ asset('css/plugins.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/magnifying-popup.css') }}">
     <link href="//cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" />
+
+    <link rel="stylesheet" href="{{ asset('css/rt-icon.css') }}">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     @stack('styles')
 </head>
 <body>
+    <div class="loader-wrapper">
+        <div class="loader">
+        </div>
+        <div class="loader-section section-left"></div>
+        <div class="loader-section section-right"></div>
+    </div>
     <!-- Header / Navigation -->
     <header class="{{ request()->is('/') ? 'home-header' : '' }}">
         @include('layouts._partials.nav')
