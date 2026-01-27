67fd1616b565008dc5c15235c9b8ecbeee915b1a
unknown <anubhav.abstain@gmail.com>
2026-01-27 17:50:12 +0530

    Update view services
    Update view services

--- resources/views/front/services/index.blade.php
+++ resources/views/front/services/index.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.front')
 
 @section('title', $page->meta_title ?? '')
-
 @section('meta_description', $page->meta_description ?? '')
 @section('meta_keywords', $page->meta_keywords ?? '')
 
