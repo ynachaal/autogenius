642ee388fba07843a11dd45b636a92f00c2e27ed
unknown <anubhav.abstain@gmail.com>
2026-01-27 11:39:37 +0530

    Update brand not showing correctly
    Update brand not showing correctly

--- resources/views/components/brand-swiper.blade.php
+++ resources/views/components/brand-swiper.blade.php
@@ -1,15 +1,11 @@
-<div class="swiper-container">
-    <div class="swiper-wrapper">
-        @foreach($brands as $brand)
-            <div class="swiper-slide">
-                <div class="company-logo">
-                    @if($brand->image)
-                        <img src="{{ asset('/' . $brand->image) }}" alt="{{ $brand->name }}">
-                    @else
-                        <img src="{{ asset('images/placeholder-car.jpg') }}" alt="Placeholder Image">
-                    @endif
-                </div>
-            </div>
-        @endforeach
+@foreach($brands as $brand)
+    <div class="swiper-slide">
+        <div class="company-logo">
+            @if($brand->image)
+                <img src="{{ asset('/' . $brand->image) }}" alt="{{ $brand->name }}">
+            @else
+                <img src="{{ asset('images/placeholder-car.jpg') }}" alt="Placeholder Image">
+            @endif
+        </div>
     </div>
-</div>
+@endforeach
