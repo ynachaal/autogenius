7172705ae8701fd32c857d2ecc10bfb8782e637c
Jatinabstain <jatin.abstain@gmail.com>
2026-02-04 13:50:13 +0530

    design updates

--- resources/views/front/pages/show.blade.php
+++ resources/views/front/pages/show.blade.php
@@ -25,7 +25,8 @@
     <section class="page-detail py-5">
         <div class="container">
             {{-- Page Title --}}
-            <h1 class="mb-4">{{ $page->title }}</h1>
+
+            {{-- <h1 class="mb-4">{{ $page->title }}</h1>--}}
 
             {{-- Page Content --}}
             <div class="page-content">
