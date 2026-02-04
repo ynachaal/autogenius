ab1b0068f2e6b9a063f289318322481aa8db3003
unknown <anubhav.abstain@gmail.com>
2026-02-04 10:30:33 +0530

    Update menu and service featured count
    Update menu and service featured count

--- app/Http/Controllers/LeadController.php
+++ app/Http/Controllers/LeadController.php
@@ -2,15 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\ProfileUpdateRequest;
+
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\View\View;
-use App\Services\PageService;
-use App\Models\Page;
 
 class LeadController extends Controller
 {
+    public function index(): View
+    {
+
+
+        return view('front.lead.index', );
+    }
 }
